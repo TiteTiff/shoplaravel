fbc699f5e2ecfe15c0a1e6cfc72195652b408286
Tite Tiff <simonchautemps.tiffanie@gmail.com>
2026-02-02 15:49:30 +0100

    Partie 2-Exercice 1-Première vue Blade

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,9 +8,8 @@ class PageController extends Controller
 {
     public function home()
     {
-        return redirect()->route('products.show',['id' =>42]);
+        return view('hello');
     }
-
     public function about()
     {
         return ('This e-shop is designed for polers who wants high quality polewear.');
---
+++ resources/views/hello.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+    <h1>E-Shop for polewear</h1>
+    <p>Welcome to our e-shop for polewear. You will find everything to be able to pole without stressing out!</p>
+</body>
+</html>
