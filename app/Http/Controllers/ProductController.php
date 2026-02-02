65776eb76a1a880cb0e96df16403308568d57e83
Tite Tiff <simonchautemps.tiffanie@gmail.com>
2026-02-02 13:45:16 +0100

    Jour 1-Exercice 3 - Route avec paramètre

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function show($id)
+    {
+        return ("Détails du produit $id");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,9 @@ use App\Http\Controllers\PageController;
 
 Route::get('/home', [PageController::class, 'home']);
 Route::get('/about', [PageController::class, 'about']);
+
+use App\Http\Controllers\ProductController;
+
+Route::get('/products/{id}', function ($id) {
+    return 'product ' .$id;
+});
